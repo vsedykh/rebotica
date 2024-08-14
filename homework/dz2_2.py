22a47a33e0874ea75eefa8f09e2c542ef06af65a
Valery Sedykh <valery.sedykh@kaspersky.com>
2024-08-14 09:47:06 +0300

    dz2 rework

--- homework/dz2_2.py
+++ homework/dz2_2.py
@@ -1,4 +1,9 @@
-a = input()
-b = input()
-m = input()
-print("ФИО:", a, b, m)
+# a = input()
+# b = input()
+# m = input()
+# print("ФИО:", a, b, m)
+
+l = "Седых"
+f = "Платон"
+i = "Валерьевич"
+print("ФИО: " + l + " " + f + " " + i)
