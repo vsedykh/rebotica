66e6b7e6e32060ce7502f827e39806925b69ef31
Valery Sedykh <valery.sedykh@gmail.com>
2024-08-31 18:00:43 +0300

    dz5

--- homework/dz5_2.py
+++ homework/dz5_2.py
@@ -5,4 +5,4 @@ qwerty = {
     "Ъкенгцыий" : "+5 678 23-41-43"
 }
 qwerty ["Ыevgsкий"]= "+ 00 0000 00 00000 0000 00"
-print(qwerty.pop("Простенький"))
+print(qwerty.keys())
---
+++ homework/dz5_4.py
@@ -0,0 +1,3 @@
+du = ["Дарья","Юра","Платон","Валерий"]
+for warp in du:
+    print(warp,len(warp))
