3ae8ed9df36767140167b3845631bf29326b52e8
Valery Sedykh <valery.sedykh@gmail.com>
2024-12-29 13:41:54 +0300

    refactor

--- class/фуклр.py
+++ class/фуклр.py
@@ -1,21 +1,42 @@
 from tkinter import *
 tkck = Tk()
-tkck.geometry("600x400")
+tkck.geometry("1800x800")
 tkck.title("Alle")
 tkck["bg"] = "red"
 callllllllllvas = Canvas(tkck,width=540,height=400,bg="white")
 callllllllllvas.grid(row= 0,column= 0,rowspan= 7)
-stateeeeeee = "circle"
+stateeeeeee = "c"
+brush = 10
+color = "red"
 def chose(input):
     global stateeeeeee
     stateeeeeee = input
+def paint(event):
+    if event.widget.__class__:
+    if stateeeeeee=="s":
+        callllllllllvas.create_rectangle(event.x - brush, event.y - brush, event.x + brush, event.y + brush, fill=color,outline=color)
+    elif stateeeeeee=="c":
+        callllllllllvas.create_oval(event.x - brush, event.y - brush, event.x + brush, event.y + brush, fill=color,outline=color)
+    elif stateeeeeee=="l1":
+        callllllllllvas.create_line(event.x - brush, event.y - brush, event.x + brush, event.y + brush, fill=color)
+    elif stateeeeeee=="l2":
+        callllllllllvas.create_line(event.x + brush, event.y - brush, event.x - brush, event.y + brush, fill=color)
 
-btn = Button(tkck, text="🟥", font=(None, 20),command=lambda:chose("square"))
-btn.grid(row=0,column=1)
-btn = Button(tkck, text="🟥", font=(None, 20),command=lambda:chose("square"))
-btn.grid(row=1,column=1)
-btn = Button(tkck, text="🟥", font=(None, 20))
-btn.grid(row=2,column=1)
-btn = Button(tkck, text="🟥", font=(None, 20))
-btn.grid(row=3,column=1)
+    print(event.__dict__)
+tkck.bind_all("<1>", paint)
+
+btn1 = Button(tkck, text="🟥", font=(None, 20),command=lambda: chose("s"))
+btn1.grid(row=0,column=1)
+btn2 = Button(tkck, text="🔴", font=(None, 20),command=lambda: chose("c"))
+btn2.grid(row=1,column=1)
+btn3 = Button(tkck, text=" ↘ ", font=(None, 20),command=lambda: chose("l1"))
+btn3.grid(row=2,column=1)
+btn4 = Button(tkck, text=" ↙ ", font=(None, 20),command=lambda: chose("l2"))
+btn4.grid(row=3,column=1)
+btn5 = Button(tkck, text="➕", font=(None, 20),command=lambda: chose("+"))
+btn5.grid(row=4,column=1)
+btn6 = Button(tkck, text="➖", font=(None, 20),command=lambda: chose("-"))
+btn6.grid(row=5,column=1)
+label = Label(tkck,text=brush, fg=color, font=(None, 20))
+label.grid(row=6, column=1)
 tkck.mainloop()
